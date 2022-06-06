357ce356e77329257d39dd1de164eb1a47ccf14c
Sean Geoghegan <seangeo@gmail.com>
2022-06-07 00:52:29 +0930

    Initial parser working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process;
+use std::fs;
+use std::error::Error;
+
+mod vm;
+
+fn parse_args(args: &[String]) -> Result<String, &'static str> {
+    if args.len() > 1 {
+        Ok(args[1].clone())
+    } else {
+        Err("not enough arguments")
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let source_file = parse_args(&args).unwrap_or_else(|err| {
+        println!("Argument Error: {}", err);
+        println!("Usage: hack_vmtranslator <vmfile>");
+        process::exit(1);
+    });
+    let vm_source = fs::read_to_string(&source_file)?;
+    let ast = vm::parse_source(&vm_source)?;
+
+    println!("source file = {}", source_file);
+    println!("ast = {:#?}", ast);
+
+    Ok(())
 }
---
+++ src/vm.rs
@@ -0,0 +1,133 @@
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub enum Segment {
+    Constant
+}
+
+impl FromStr for Segment {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Segment, String> {
+        match s {
+            "constant" => Ok(Segment::Constant),
+            _ => Err(format!("Unknown segment name: '{}'", s))
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Command {
+    Push { segment: Segment, index: u16 },
+    Pop  { segment: Segment, index: u16 },
+    Add,
+    Sub,
+    Neg,
+    Eq,
+    Gt,
+    Lt,
+    And,
+    Or,
+    Not
+}
+
+impl FromStr for Command {
+    type Err = String;
+
+    fn from_str(line: &str) -> Result<Command, String> {
+        Command::from_str(line)
+    }
+}
+
+impl Command {
+    fn from_str(line: &str) -> Result<Command, String> {
+        if let Some(s) = line.strip_prefix("push") {
+            Command::parse_push(s.trim())
+        } else if line == "add" {
+            Ok(Command::Add)
+        } else if line == "sub" {
+            Ok(Command::Sub)
+        } else if line == "neg" {
+            Ok(Command::Neg)
+        } else if line == "eq" {
+            Ok(Command::Eq)
+        } else if line == "lt" {
+            Ok(Command::Lt)
+        } else if line == "gt" {
+            Ok(Command::Gt)
+        } else if line == "and" {
+            Ok(Command::And)
+        } else if line == "or" {
+            Ok(Command::Or)
+        } else if line == "not" {
+            Ok(Command::Not)
+        } else {
+            Err(format!("Parser not implemented for '{}'", line))
+        }
+    }
+
+    fn parse_push(s: &str) -> Result<Command, String> {
+        let parts: Vec<&str> = s.split_whitespace().collect();
+
+        if parts.len() == 2 {
+            let segment = parts[0].parse::<Segment>();
+            let index = parts[1].parse::<u16>();
+
+            if segment.is_err() {
+                Err(segment.unwrap_err())
+            } else if index.is_err() {
+                Err(format!("Error parsing index: {}", index.unwrap_err()))
+            } else {
+                Ok(Command::Push { segment: segment.unwrap(), index: index.unwrap() })
+            }
+        } else {
+            Err(format!("push expected format 'push <segment> <index>'"))
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct SourceCommand<'a> {
+    line: usize,
+    command: Command,
+    source: &'a str
+}
+
+pub fn parse_source(source: &str) -> Result<Vec<SourceCommand>, String> {
+    source.lines().enumerate().filter_map(|(i, line)| {
+        match strip_comments(line) {
+            Some(s) => Some(parse_source_command(i, s)),
+            None    => None
+        }
+    }).collect()
+}
+
+fn strip_comments(line: &str) -> Option<&str> {
+    let code = match line.find("//") {
+        None => line,
+        Some(i) => {
+            let (code, _) = line.split_at(i);
+            code
+        }
+    }.trim();
+
+    if code.is_empty() {
+        None
+    } else {
+        Some(code)
+    }
+}
+
+fn parse_source_command<'a>(i: usize, source: &'a str) -> Result<SourceCommand<'a>, String> {
+    match source.parse::<Command>() {
+        Ok(command) => Ok(
+            SourceCommand{
+                line: i,
+                command: command,
+                source: source
+            }
+        ),
+        Err(e) => Err(format!("Parse error at line {} ({}): {}", i, source, e))
+    }
+}
+
