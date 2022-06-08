b00dced747807f9d15bc5ab061bf969d8248ce19
Sean Geoghegan <seangeo@gmail.com>
2022-06-08 22:21:22 +0930

    Writes to a file

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use std::env;
 use std::error::Error;
 use std::fs;
 use std::process;
+use std::path::Path;
 
-mod vm;
+pub mod vm;
+pub mod asm;
 
 fn parse_args(args: &[String]) -> Result<String, &'static str> {
     if args.len() > 1 {
@@ -22,9 +24,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
     let vm_source = fs::read_to_string(&source_file)?;
     let ast = vm::parse_source(&vm_source)?;
+    let asm = asm::generate_code(ast)?;
 
     println!("source file = {}", source_file);
-    println!("ast = {:#?}", ast);
+
+    let target_file_name = Path::new(&source_file).with_extension("asm");
+    println!("output file = {}", target_file_name.to_str().unwrap());
+    fs::write(target_file_name, asm.join("\n"))?;
 
     Ok(())
 }
